ae3e94484fc46f378397e86f43af75f28ded6dc3
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-09-07 00:47:11 +0700

    Feat(SignUp,Login&Mode):Homepage

---
+++ src/components/SignUp_Login_Dia.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    max-width="500"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        color=#FFFFDD
+        class="mr-2" 
+        text
+        v-bind="attrs"
+        v-on="on"
+      >
+        {{ IconLabel }}
+      </v-btn>
+    </template> 
+    <v-card>
+        <v-col cols="12" >
+          <v-card-title class="text-h5" style=" color:#365BC2; display: flex; justify-content: center; align-items: center;">
+            {{ title }}
+          </v-card-title>
+        </v-col>
+      <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-col
+            cols="12"
+            >
+            <text-field
+            rounded
+            v-model="email"
+            label="E-mail or Username" 
+            required
+            filled
+            small
+            class="flex"
+            
+            />
+            <text-field
+            label="Password"
+            v-model="password"
+            :type="show1 ? 'text' : 'password'"
+            required
+            rounded
+            filled
+            small
+            class="flex"
+            />
+            <slot name="forgot-pwd"/>
+            <slot name="signUp-textField"/>
+          <v-btn
+            block
+            rounded
+            color="#E84D72" 
+            class="white--text"
+            style="margin-top: 20px;"
+            >{{ BtnLabel1 }}
+          </v-btn>
+          <v-col cols="12" class="center-content">
+            <span style="color: #365BC2"> Or</span>
+          </v-col>
+            <v-btn
+            block
+            rounded
+            outlined
+            color="#365BC2"> 
+            <v-img src="../assets\Img\search.png" max-width="20" />
+            {{BtnLabel2}}
+          </v-btn>  
+        </v-col>
+    </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+// import BtnComp from './BtnComp.vue';
+import TextField from './TextField.vue';
+
+
+export default {
+  name: 'SignUp_Dia',
+  components: {
+    TextField,
+    // BtnComp
+  },
+  props: {
+    IconLabel: String,
+    title: String,
+    BtnLabel1: String,
+    BtnLabel2 : String
+  },
+  data: () => ({
+    dialog: false,
+    show1: false,
+    email: '',
+    password: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  }) 
+}
+</script>
+<style>
+.center-content {
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.Btnlogo {
+display : flex;
+justify-content: start;
+align-self: start;
+}
+</style>
--- src/views/Layout/AuthLayout.vue
+++ src/views/Layout/AuthLayout.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire" :class="{'dark-theme': darkTheme}">
     <v-app-bar
       app
-      color="white"
+      color=#016A70
       flat
     >
       <v-container class="px-0 fill-height">
@@ -10,8 +10,10 @@
         <v-avatar
           class="mr-10"
           color="grey darken-1"
-          size="40"
-        ></v-avatar>
+          size="50"
+        >
+        <v-img src="../../assets/Img/User/mony.jpg"/>
+      </v-avatar>
 
         <v-btn
           v-for="link in links"
@@ -19,15 +21,37 @@
           :to="link.path"
           text
           class="font-weight-bold"
+          color=#FFFFDD
 
         >
           {{ link.name }}
         </v-btn>
         <v-spacer></v-spacer>
         <v-responsive max-width="260">
-          <text-field label="Search" color="primary" prepend-inner-icon="mdi-magnify"/>
+          <text-field label="Search" color=#FFFFDD prepend-inner-icon="mdi-magnify"/>
         </v-responsive>
-        <btn-comp @click="toggleTheme" BtnLabel="Mode" style="margin-right: 10px;" ></btn-comp>
+        <SL_Dia IconLabel="Login" title="Login"  BtnLabel1="Login" BtnLabel2="Continue with google">
+          <template v-slot:forgot-pwd>
+            <router-link to="/" class="router-link" style: style="display: flex; justify-content: end; align-items: end;">forgot password ?</router-link>
+          </template>
+        </SL_Dia>
+        <SL_Dia IconLabel="Sign Up" title="Sign Up"  BtnLabel1="Sign Up" BtnLabel2="Sign Up with google">
+          <template v-slot:signUp-textField>
+            <v-text-field
+            v-model="password"
+            :type="show1 ? 'text' : 'password'"
+            label="Confirm password"
+            required
+            rounded
+            filled
+            small
+            class="flex"/>
+          </template>
+        </SL_Dia>
+        <!-- <btn-comp @click="toggleTheme" BtnLabel="Mode" style="margin-right: 10px;"  Btncolor="#E84D72"/> -->
+        <v-btn @click="toggleTheme" icon :color="Btncolor" style="margin-right: 10px;">
+          <v-icon>{{ darkTheme ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+        </v-btn>
       </v-row>
       </v-container>
     </v-app-bar>
@@ -71,13 +95,13 @@
             <v-sheet min-height="100vh" rounded="lg">
               <v-row>
                 <v-col cols="12" md="8">
-                  <v-img max-height="150" max-width="400" src="../../assets/Img/Pie.png"/>
+                  <v-img max-height="150" height="600" width="600" src="../../assets/Img/Pie.png"/>
                   <v-row style="margin-top: 20px;">
                     <v-col cols="6" md="2">
-                      <Btn-dia BtnLabel="Turn On" title="Turn off " icon="mdi-watering-can" text="Successfully Turn On Valve !"/>
+                      <Btn-dia BtnLabel="Turn On" title="Turn off " BtnColor="#016A70" icon="mdi-watering-can" text="Successfully Turn On Valve !"/>
                     </v-col>
                     <v-col cols="6" md="2">
-                      <Btn-dia BtnLabel="Turn Off" title="Turn on valve !" icon="mdi-emoticon" text="Successfully Turn Off Valve !"/>
+                      <Btn-dia BtnLabel="Turn Off" title="Turn on valve !"  BtnColor="#016A70" icon="mdi-emoticon" text="Successfully Turn Off Valve !"/>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -95,24 +119,20 @@
 
 <script>
 import WeatherCard from '../../components/WeatherCard.vue'
-import BtnComp from '../../components/BtnComp.vue'
+// import BtnComp from '../../components/BtnComp.vue'
 import BtnDia from '../../components/DialogVue.vue'
 import TextField from '../../components/TextField.vue'
+import SL_Dia from '../../components/SignUp_Login_Dia.vue'
 
   export default {
   components: { 
     WeatherCard,
     BtnDia,
     TextField,
-    BtnComp
+    // BtnComp,
+    SL_Dia
   },
     name: 'AuthLayout',
-    component: {
-      WeatherCard,
-      BtnComp,
-      BtnDia,
-      TextField
-    },
     data: () => ({
       links: [
         // 'Dashboard',
